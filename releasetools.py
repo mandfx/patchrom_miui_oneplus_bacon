aba0ba747dfec5e5f228fb5258cfd9c6429abefc
X-s <xsmumu@gmail.com>
2015-04-27 23:09:54 +0800

    add file_contexts

--- releasetools.py
+++ releasetools.py
@@ -29,10 +29,18 @@ def DeleteSystem(info):
       edify.script[i] = 'delete_recursive("/system");'
       return
 
+def WritePolicyConfig(info):
+  try:
+    file_contexts = info.input_zip.read("META/file_contexts")
+    common.ZipWriteStr(info.output_zip, "file_contexts", file_contexts)
+  except KeyError:
+    print "warning: file_context missing from target;"
+	
 def FullOTA_InstallEnd(info):
     RemoveDeviceGetprop(info)
     MountSystem(info)
     DeleteSystem(info)
+    WritePolicyConfig(info)
 
 def IncrementalOTA_InstallEnd(info):
     RemoveDeviceAssert(info)
