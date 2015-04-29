bc19c3df993e9a06e59b4423e26c71bce99afb2f
X-s <xsmumu@gmail.com>
2015-04-29 22:14:00 +0800

    add auto_make.sh

---
+++ auto_make.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+#sh for miui patchrom
+#usage:
+#. make.sh $1 $2 $3
+#$1 == Device
+#$2 == BUILD_NUMBER
+#$3 == Last_Target.zip
+echo "******************"
+echo "*Init Environment*"
+echo "******************"
+cd ..
+. build/envsetup.sh
+cd $1
+pwd
+echo "******************"
+echo "*     Clean      *"
+echo "******************"
+make clean
+echo "******************"
+echo "*     Fullota    *"
+echo "******************"
+make fullota BUILD_NUMBER=$2
+echo "******************"
+echo "*Copy Fullota.zip*"
+echo "******************"
+cp out/fullota.zip $2-fullota.zip
+echo "******************"
+echo "*      OTA       *"
+echo "******************"
+echo "------------------"
+echo "    make ota?     "
+echo "------------------"
+read
+../tools/releasetools/ota_from_target_files -k ../build/security/testkey -i $3 out/target_files.zip OTA-$3-$2.zip
+echo "******************"
+echo "*Copy  Target.zip*"
+echo "******************"
+cp out/target_files.zip $2-target.zip
+echo "******************"
+echo "*     Done!      *"
+echo "******************"
