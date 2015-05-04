63a1a318631e2f535155dc16f0b1115062f58ff3
X-s <xsmumu@gmail.com>
2015-05-04 16:34:50 +0800

    update releasetools.py and color1.2 based

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,6 @@
 import common
 import edify_generator
+import os
 
 def RemoveDeviceAssert(info):
   edify = info.script
@@ -36,63 +37,36 @@ def WritePolicyConfig(info):
   except KeyError:
     print "warning: file_context missing from target;"
     
-def InstallImage(img_name, img_file, partition, info):
-  common.ZipWriteStr(info.output_zip, img_name, img_file)
-  info.script.AppendExtra(('package_extract_file("' + img_name + '", "' + partition + '");'))
+def InstallBased(info):
+    for filename in os.listdir("other"):
+        if not (filename.find('.mbn')==-1 and filename.find('.bin')==-1 and filename.find('reserve4')==-1):
+            data=open(os.path.join(os.getcwd(),"other",filename)).read()
+            common.ZipWriteStr(info.output_zip, filename, data)
 
-def InstallAboot(info):
-  img_file = info.input_zip.read("BOOTABLE_IMAGES/emmc_appsboot.mbn")
-  info.script.Print("Writing aboot image...")
-  InstallImage("emmc_appsboot.mbn", img_file, "/dev/block/platform/msm_sdcc.1/by-name/aboot", info)
-  
-def InstallModem(info):
-  img_file = info.input_zip.read("BOOTABLE_IMAGES/NON-HLOS.bin")
-  info.script.Print("Writing modem image...")
-  InstallImage("NON-HLOS.bin", img_file, "/dev/block/platform/msm_sdcc.1/by-name/modem", info)
-
-def InstallSbl1(info):
-  img_file = info.input_zip.read("BOOTABLE_IMAGES/sbl1.mbn")
-  info.script.Print("Writing sbl1 image...")
-  InstallImage("sbl1.mbn", img_file, "/dev/block/platform/msm_sdcc.1/by-name/sbl1", info)
-
-def InstallRpm(info):
-  img_file = info.input_zip.read("BOOTABLE_IMAGES/rpm.mbn")
-  info.script.Print("Writing rpm image...")
-  InstallImage("rpm.mbn", img_file, "/dev/block/platform/msm_sdcc.1/by-name/rpm", info)
-
-def InstallTz(info):
-  img_file = info.input_zip.read("BOOTABLE_IMAGES/tz.mbn")
-  info.script.Print("Writing tz image...")
-  InstallImage("tz.mbn", img_file, "/dev/block/platform/msm_sdcc.1/by-name/tz", info)
-
-def InstallStatic_nvbk(info):
-  img_file = info.input_zip.read("BOOTABLE_IMAGES/static_nvbk.bin")
-  info.script.Print("Writing static_nvbk image...")
-  InstallImage("static_nvbk.bin", img_file, "/dev/block/platform/msm_sdcc.1/by-name/oppostanvbk", info)
-
-def InstallReserve4(info):
-  img_file = info.input_zip.read("BOOTABLE_IMAGES/reserve4.img")
-  info.script.Print("Writing reserve4 image...")
-  InstallImage("reserve4.img", img_file, "/dev/block/platform/msm_sdcc.1/by-name/reserve4", info)
-
-def InstallLogo(info):
-  img_file = info.input_zip.read("BOOTABLE_IMAGES/logo.bin")
-  info.script.Print("Writing logo image...")
-  InstallImage("logo.bin", img_file, "/dev/block/platform/msm_sdcc.1/by-name/LOGO", info)
+    extra_img_flash = """ui_print("Writing aboot image...");
+package_extract_file("emmc_appsboot.mbn", "/dev/block/platform/msm_sdcc.1/by-name/aboot");
+ui_print("Writing modem image...");
+package_extract_file("NON-HLOS.bin", "/dev/block/platform/msm_sdcc.1/by-name/modem");
+ui_print("Writing sbl1 image...");
+package_extract_file("sbl1.mbn", "/dev/block/platform/msm_sdcc.1/by-name/sbl1");
+ui_print("Writing rpm image...");
+package_extract_file("rpm.mbn", "/dev/block/platform/msm_sdcc.1/by-name/rpm");
+ui_print("Writing tz image...");
+package_extract_file("tz.mbn", "/dev/block/platform/msm_sdcc.1/by-name/tz");
+ui_print("Writing static_nvbk image...");
+package_extract_file("static_nvbk.bin", "/dev/block/platform/msm_sdcc.1/by-name/oppostanvbk");
+ui_print("Writing logo image...");
+package_extract_file("logo.bin", "/dev/block/platform/msm_sdcc.1/by-name/LOGO");
+ui_print("Writing reserve4 image...");
+package_extract_file("reserve4.img", "/dev/block/platform/msm_sdcc.1/by-name/reserve4");"""
+    info.script.AppendExtra(extra_img_flash);
 	
 def FullOTA_InstallEnd(info):
     RemoveDeviceGetprop(info)
     MountSystem(info)
     DeleteSystem(info)
     WritePolicyConfig(info)
-    InstallAboot(info)
-    InstallModem(info)
-    InstallSbl1(info)
-    InstallRpm(info)
-    InstallTz(info)
-    InstallStatic_nvbk(info)
-    InstallReserve4(info)
-    InstallLogo(info)
+    InstallBased(info)
 
 def IncrementalOTA_InstallEnd(info):
     RemoveDeviceAssert(info)
