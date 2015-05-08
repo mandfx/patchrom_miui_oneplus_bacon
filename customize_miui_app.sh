996c17bbc1012ff8bff8e869f4891174e71eec65
X-s <xsmumu@gmail.com>
2015-05-09 02:34:58 +0800

    update StockSettings Icon Patch

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -49,6 +49,7 @@ fi
 if [ $1 = "Settings" ];then
 	applyPatch $1 $2
 	$XMLMERGYTOOL $1/res/values $2/res/values
+	$XMLMERGYTOOL $1/res/values-zh-rCN $2/res/values-zh-rCN
 fi
 
 if [ $1 = "SecurityCenter" ];then
