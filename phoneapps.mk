a1282f0e495289d9946593fe0e4cfbfb36a6a828
X-s <imxs1107@gmail.com>
2016-11-16 16:47:37 +0800

    update phoneapps.mk

--- phoneapps.mk
+++ phoneapps.mk
@@ -1,20 +1,16 @@
 local-phone-apps = $(private-phone-apps)
 local-phone-priv-apps = $(private-phone-priv-apps)
 
-private-phone-apps := AntHalService BasicDreams Bluetooth BluetoothExt BluetoothMidiService Calendar \
-				CaptivePortalLogin CertInstaller CMFileManager CMWallpapersConfigPanel DeskClock Development \
-				DocumentsUI DownloadProviderUi Eleven Email ExactCalculator Exchange2 Galaxy4 Gallery2 Gello \
-				HoloSpiralWallpaper HTMLViewer KeyChain LatinIME LiveWallpapers LiveWallpapersPicker LockClock \
-				messaging NfcNci NoiseField PacProcessor PhaseBeam PhotoPhase PhotoTable PicoTts PrintSpooler \
-				Profiles Provision shutdownlistener Snap SoundRecorder Stk telresources Terminal TimeService \
-				UserDictionaryProvider WallpaperPicker WAPPushManager webview
+private-phone-apps := AntHalService BasicDreams Bluetooth BluetoothExt BluetoothMidiService \
+				CertInstaller Galaxy4 HoloSpiralWallpaper HTMLViewer KeyChain LatinIME LiveWallpapers \
+				NfcNci NoiseField PacProcessor PhaseBeam PhotoPhase PhotoTable PicoTts PrintSpooler \
+				shutdownlistener Snap Stk telresources TimeService \
+				UserDictionaryProvider WAPPushManager webview
 
-private-phone-priv-apps := AudioFX BackupRestoreConfirmation CalendarProvider CallLogBackup CarrierConfig \
-				CellBroadcastReceiver CMAudioService CMBugReport CMSettingsProvider CMUpdater CNEService \
-				com.qualcomm.location Contacts ContactsProvider CyanogenSetupWizard DataUsageProvider \
-				DefaultContainerService Dialer DownloadProvider dpmserviceapp ExternalStorageProvider \
-				FusedLocation InputDevices LiveLockScreenService ManagedProvisioning MediaProvider \
-				MmsService OneTimeInitializer PackageInstaller ProxyHandler qcrilmsgtunnel QtiTetherService \
-				Screencast Settings SettingsProvider SharedStorageBackup Shell StatementService SystemUI Tag \
-				Telecom TelephonyProvider TeleService ThemeChooser ThemeManagerService ThemesProvider Trebuchet \
-				VpnDialogs WallpaperCropper WeatherManagerService WeatherProvider
+private-phone-priv-apps := AudioFX BackupRestoreConfirmation \
+				CellBroadcastReceiver CMAudioService CMSettingsProvider CNEService \
+				com.qualcomm.location DataUsageProvider \
+				DefaultContainerService dpmserviceapp ExternalStorageProvider \
+				FusedLocation InputDevices OneTimeInitializer ProxyHandler \
+				qcrilmsgtunnel QtiTetherService SharedStorageBackup Shell \
+				StatementService Tag ThemesProvider
