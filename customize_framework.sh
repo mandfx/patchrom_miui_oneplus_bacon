4241a25b4315dbe6a91b7eb4217968953a6746b0
X-s <imxs1107@gmail.com>
2016-12-29 01:27:55 +0800

    update patch with miui, fix appops ids

--- customize_framework.sh
+++ customize_framework.sh
@@ -2,6 +2,22 @@
 
 OUT_MIUI_FRAMEWORK_DIR=$1
 OUT_FRAMEWORK_DIR=$2
+GIT_APPLY=$PORT_ROOT/tools/git.apply
+
+function applyPatch () {
+    for patch in `find $1 -name *.patch`
+    do
+        cp $patch out/
+        cd out
+        $GIT_APPLY `basename $patch`
+        for rej in `find $2 -name *.rej`
+        do
+          echo "Patch $patch fail"
+          exit 1
+        done
+        cd ..
+      done
+}
 
 if [ "$OUT_FRAMEWORK_DIR" == "out/framework" ];then
     cp $OUT_MIUI_FRAMEWORK_DIR/smali/android/widget/Editor*.smali $OUT_FRAMEWORK_DIR/smali/android/widget/
@@ -13,4 +29,5 @@ fi
 
 if [ "$OUT_FRAMEWORK_DIR" == "out/services" ];then
     cp $OUT_MIUI_FRAMEWORK_DIR/smali/com/android/server/power/ShutdownThread*.smali $OUT_FRAMEWORK_DIR/smali/com/android/server/power/
+    applyPatch "overlay/services"
 fi
